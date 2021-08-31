1d64590f66414f45846b3ee551718347168f5bde
abschill <ajay198810@gmail.com>
2021-08-31 09:05:01 -0400

    default proj 0.0.3

--- packages/default/webpack.config.js
+++ packages/default/webpack.config.js
@@ -1,4 +1,26 @@
-const path = require( 'path' );
-module.exports = {
 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+module.exports = {
+    devtool:'inline-source-map',
+    entry:{
+        index: './src/index.js'
+    },
+    module:{
+        rules:[
+            {
+                test:/\.js$/,
+                use:'babel-loader',
+                exclude:/node_modules/
+            }
+        ]
+    },
+    output:{
+        filename:'[name].js',
+        path:path.resolve(__dirname, 'dist'),
+    },
+    target:'web',
+    plugins:[
+         new CleanWebpackPlugin()
+     ]
 }
