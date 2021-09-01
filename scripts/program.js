71bc961f5ff5c6559d5f155a98a2cf2185294257
abschill <ajay198810@gmail.com>
2021-09-01 14:48:30 -0400

    updates to package and deps

--- scripts/program.js
+++ scripts/program.js
@@ -1,14 +1,15 @@
 const Logger = require( '../util/logger' );
 const defaultTemplate = require( '../bin/default' );
 const path = require( 'path' );
-const genPath = (i) => {
+const shell = require( 'shelljs' );
+const genPath = ( i ) => {
     if( i === '.'  || i === '/' || i === undefined || i === null ){
         return process.cwd();
     }
     else {
         if( !i.includes( '.' ) ){
             Logger.error( 'Path Error: Must use relative path' );
-            return process.exit(1);
+            return process.exit( 1 );
         }
         else {
             return path.join( process.cwd(), i );
@@ -17,13 +18,21 @@ const genPath = (i) => {
 }
 const setup = ( args ) => {
     try{
-        const path = genPath( args[0] );
-        Logger.status( `Building Project at ${path}`)
+        const _path = genPath( args[0] );
+        Logger.status( `Building Project at ${_path}` )
         Logger.split();
         if( args[1] ) {
             Logger.status( `Chosen Template: ${args[1]}` );
         }
-        Logger.heading( 'Success' )
+        else {
+            Logger.status( 'No Template Chosen ... using default' );
+            defaultTemplate(_path);
+            shell.cd(_path);
+            Logger.status( 'Installing Packages..' );
+            shell.ls().exec( 'yarn install' )
+        }
+        Logger.heading( 'Success' );
+        process.exit( 0 );
     }
     catch ( e ) {
         Logger.error( e );
--- util/logger.js
+++ util/logger.js
@@ -4,9 +4,7 @@ const status = (msg) => msg?console.log( chalk.blue(msg) ) : console.log();
 const error = (msg) => msg?console.log( chalk.red( msg ) ): console.log();
 const success = (msg) => msg?console.log( chalk.green( msg ) ) :console.log();
 const split = () => console.log( chalk.cyan('~~~~~~~~~~~~~~~~'));
-
 const heading = (hdg) => console.log( chalk.cyan( figlet.textSync(hdg) ) );
-
 const Logger = {
     status,
     error,
